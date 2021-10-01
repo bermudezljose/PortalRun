3626c04cf2693ab78916ee2f75781b42fa3a6394
bermudezljose <70238005+bermudezljose@users.noreply.github.com>
2021-10-01 17:54:20 -0500

    Add files via upload

---
+++ Grounded.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    GameObject Player1; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player1 = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Grass")
+        {
+            Player1.GetComponent<PlayerMovement>().isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Grass")
+        {
+            Player1.GetComponent<PlayerMovement>().isGrounded = false;
+
+        }
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public bool isGrounded = false;
+    private Rigidbody2D body;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+
+    private void Awake()
+    {
+        // getComponent gives us acess to Rigidbody2D
+        body = GetComponent<Rigidbody2D>();
+
+        // getComponent gives us acess to Animator
+        anim = GetComponent<Animator>();
+
+        // This will stop the rotation of players when colliding against one another.
+        body.freezeRotation = true;
+
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 10f), ForceMode2D.Impulse);
+        }
+    }
+}
